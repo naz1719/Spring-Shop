43a360e6ef0ac0f1024d748f782a1357c3fc3566
nazar <khimin1719@gmail.com>
2017-02-26 10:59:48 +0200

    add pagination

--- src/main/java/com/khimin/shop/repositories/ProductRepository.java
+++ src/main/java/com/khimin/shop/repositories/ProductRepository.java
@@ -4,10 +4,11 @@ package com.khimin.shop.repositories;
 import com.khimin.shop.entities.Product;
 import org.bson.types.ObjectId;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
 
-public interface ProductRepository extends CrudRepository<Product, ObjectId> {
+public interface ProductRepository extends PagingAndSortingRepository<Product, ObjectId> {
 	List<Product> findByName(String name);
 
 	List<Product> findByNameLike(String name);
