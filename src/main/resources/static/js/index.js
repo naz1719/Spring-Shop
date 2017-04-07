905fb5846928d7f9487633081c12ca2da4761f79
Nazar Khimin <khimin1719@gmail.com>
2017-04-07 18:44:12 +0300

    crud. was added ui

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,1 +1,5 @@
 var myApp = angular.module('myApp', ['ngMaterial','ngMessages']);
+myApp.config(function ($mdThemingProvider) {
+    $mdThemingProvider.theme('green', 'default')
+        .primaryPalette('green')
+});
