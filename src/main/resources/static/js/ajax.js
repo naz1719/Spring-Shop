fc02507b67a528956d2c66c0ee91e4b46f2a5306
Nazar Khimin <khimin1719@gmail.com>
2017-04-09 19:59:05 +0300

    ajax

--- src/main/resources/static/js/ajax.js
+++ src/main/resources/static/js/ajax.js
@@ -1,22 +1,33 @@
-/**
- * Created by nazar on 4/9/17.
- */
+// /**
+//  * Created by nazar on 4/9/17.
+//  */
+//
+// $(document).ready(function () {
+//     $("#allProducts").click(function () {
+//         $.ajax({
+//             url: "products"
+//         })
+//             .done(function (result) {
+//                 $("#products").html(result);
+//             });
+//     });
+//
+// });
 
-$(document).ready(function () {
-    $("#allProducts").click(function () {
-        $.ajax({
-            url: "products"
-        })
-            .done(function (result) {
-                $("#products").html(result);
-            });
-    });
-    $("#newProduct").click(function () {
-        $.ajax({
-            url: "/product/new"
-        })
-            .done(function (result) {
-                $("#products").html(result);
-            });
-    });
-});
+// myApp.controller('Request', function($scope,$http)
+// {
+//     $scope.cloc = function () {
+//         $http({
+//             method: 'GET',
+//             url: '/products'
+//         }).then(function successCallback(response) {
+//             console.log("success")
+//             $scope.response = response;
+//             // this callback will be called asynchronously
+//             // when the response is available
+//         }, function errorCallback(response) {
+//             // called asynchronously if an error occurs
+//             // or server returns response with an error status.
+//         });
+//     };
+// });
--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -3,21 +3,3 @@ myApp.config(function ($mdThemingProvider) {
     $mdThemingProvider.theme('green', 'default')
         .primaryPalette('green')
 });
-
-// myApp.controller('Request', function($scope,$http)
-// {
-//     $scope.cloc = function () {
-//         $http({
-//             method: 'GET',
-//             url: '/products'
-//         }).then(function successCallback(response) {
-//             console.log("success")
-//             $scope.response = response;
-//             // this callback will be called asynchronously
-//             // when the response is available
-//         }, function errorCallback(response) {
-//             // called asynchronously if an error occurs
-//             // or server returns response with an error status.
-//         });
-//     };
-// });
