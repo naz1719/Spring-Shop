6e1a25036ad663d856d65a4e225d464a6c5d0462
Nazar Khimin <khimin1719@gmail.com>
2017-04-11 18:43:58 +0300

    added log in

--- src/main/java/com/khimin/shop/MvcConfig.java
+++ src/main/java/com/khimin/shop/MvcConfig.java
@@ -12,6 +12,7 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("index");
+        registry.addViewController("/home").setViewName("index");
         registry.addViewController("/hello").setViewName("hello");
         registry.addViewController("/login").setViewName("login");
     }
--- src/main/java/com/khimin/shop/WebSecurityConfig.java
+++ src/main/java/com/khimin/shop/WebSecurityConfig.java
@@ -15,7 +15,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
+                .antMatchers("/","/home", "/products","/product/{id}").permitAll()
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
