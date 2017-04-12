f843fee180cd77c50dad4b8b873b4ef128f969dd
Nazar Khimin <khimin1719@gmail.com>
2017-04-12 21:53:48 +0300

    config sec

--- src/main/java/com/khimin/shop/WebSecurityConfig.java
+++ src/main/java/com/khimin/shop/WebSecurityConfig.java
@@ -15,7 +15,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/","/home", "/products","/users").permitAll()
+                .antMatchers("/", "/products","/users").permitAll()
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
