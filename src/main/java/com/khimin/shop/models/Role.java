213adc2941c681ceecf0637211892419271133c2
Nazar Khimin <khimin1719@gmail.com>
2017-04-14 09:06:52 +0300

    was added role

---
+++ src/main/java/com/khimin/shop/models/Role.java
@@ -0,0 +1,8 @@
+package com.khimin.shop.models;
+
+/**
+ * Created by nazar on 4/14/17.
+ */
+public enum Role {
+    USER, ADMIN
+}
--- src/main/java/com/khimin/shop/models/User.java
+++ src/main/java/com/khimin/shop/models/User.java
@@ -3,10 +3,7 @@ package com.khimin.shop.models;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 /**
  * Created by nazar on 4/12/17.
@@ -17,11 +14,18 @@ import javax.persistence.Id;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false, updatable = false)
     private Long id;
 
     private String username;
+
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
     private String password;
+
+    @Column(name = "role", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role = Role.USER;
 
     public User(String username, String email, String password) {
         this.username = username;
