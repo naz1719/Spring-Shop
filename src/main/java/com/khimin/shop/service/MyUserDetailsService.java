9027fed83c5f1b3dd425b39be5065940619e96f1
Khimin <khimin1719@gmail.com>
2017-04-29 04:43:29 -0400

    log in by email

--- src/main/java/com/khimin/shop/service/MyUserDetailsService.java
+++ src/main/java/com/khimin/shop/service/MyUserDetailsService.java
@@ -31,7 +31,7 @@ public class MyUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username);
+        User user = userRepository.findByEmail(username);
         if (user == null) {
             throw new UsernameNotFoundException(username);
         }
