9044abfc60daee3a96daf8e108c69c99ffdb1b46
Your Name <you@example.com>
2017-05-02 19:05:06 +0300

    correct hibernate mapping

--- src/main/java/com/khimin/shop/SpringProjectApplication.java
+++ src/main/java/com/khimin/shop/SpringProjectApplication.java
@@ -30,9 +30,13 @@ public class SpringProjectApplication {
     public CommandLineRunner demo(ProductRepository  productRepository, UserRepository userRepository) {
         return (args) -> {
             productRepository.deleteAll();
-            productRepository.save(new Product(5, "id", "Nazar", 50,"boot.png"));
             userRepository.deleteAll();
+
+            Product pomdor = new Product(5, "id", "Nazar", 50,"boot.png");
+            productRepository.save(pomdor);
             User admin = new User("admin","nazar.khimin@gmail.com","password",Role.ADMIN,true);
+            admin.getProducts().add(pomdor);
+            pomdor.setUser(admin);
             User user = new User("user","khimin1719@gmail.com","password",Role.USER,true);
             userRepository.save(user);
             userRepository.save(admin);
--- src/main/java/com/khimin/shop/models/User.java
+++ src/main/java/com/khimin/shop/models/User.java
@@ -5,6 +5,8 @@ import lombok.NoArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -31,8 +33,9 @@ public class User {
     private Role role;
     private Boolean isAccountNonLocked;
 
-//    @OneToMany(fetch=FetchType.LAZY, mappedBy="user")
-//    private Set products;
+    @OneToMany(cascade = {CascadeType.ALL})
+    @JoinTable(name = "USER_PRODUCT", joinColumns = @JoinColumn(name="user_id"),inverseJoinColumns = @JoinColumn(name = "product_id"))
+    private Collection<Product> products = new ArrayList<Product>();
 
     public User(String username, String email, String password, Role role, boolean isAccountNonLocked) {
         this.username = username;
